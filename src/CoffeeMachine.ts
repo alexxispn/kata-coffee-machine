e97464bee2bdb85bd2ff59194b74c4ce866013b5
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-24 17:21:37 +0200

    refactor: expand to processing order

--- src/CoffeeMachine.ts
+++ src/CoffeeMachine.ts
@@ -1,9 +1,28 @@
 import { DrinkMaker } from "./DrinkMaker.js";
 
+export class ProcessingOrder {
+  private sugar: number = 0
+
+  constructor(private readonly drinkType: string) {}
+
+  addSugar() {
+    this.sugar++;
+  }
+
+  toCommand() {
+    if (this.sugar > 0) {
+      return `${this.drinkType}:${this.sugar}:0`
+    }
+
+    return `${this.drinkType}::`
+  }
+}
+
 export class CoffeeMachine {
   private drinkMaker: DrinkMaker;
   private drinkType: string = ""
   private sugar: number = 0
+  private order?: ProcessingOrder
 
   constructor(drinkMaker: DrinkMaker) {
     this.drinkMaker = drinkMaker;
@@ -12,6 +31,7 @@ export class CoffeeMachine {
   selectCoffee() {
     this.drinkType = "C";
     this.sugar = 0;
+    this.order = new ProcessingOrder("C")
   }
 
   makeDrink(): void {
@@ -22,26 +42,32 @@ export class CoffeeMachine {
     if (this.drinkType === "") {
       return `M:drink-has-not-been-selected`
     }
+    if (!this.order) {
+      return `M:drink-has-not-been-selected`
+    }
 
     if (this.sugar > 0) {
       return `${this.drinkType}:${this.sugar}:0`
     }
 
-    return `${this.drinkType}::`
+    return this.order.toCommand()
   }
 
 
   selectTea() {
     this.drinkType = "T"
     this.sugar = 0;
+    this.order = new ProcessingOrder("T")
   }
 
   selectChocolate() {
     this.drinkType = "H"
     this.sugar = 0;
+    this.order = new ProcessingOrder("H")
   }
 
   addSugar() {
     this.sugar += 1
+    return this.order?.addSugar()
   }
 }
