aeb5c3ebcec17f8ed2105eac2e65931b345b19d9
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-24 17:22:53 +0200

    refactor: contract to processing order

--- src/CoffeeMachine.ts
+++ src/CoffeeMachine.ts
@@ -20,18 +20,10 @@ export class ProcessingOrder {
 
 export class CoffeeMachine {
   private drinkMaker: DrinkMaker;
-  private drinkType: string = ""
-  private sugar: number = 0
   private order?: ProcessingOrder
 
   constructor(drinkMaker: DrinkMaker) {
     this.drinkMaker = drinkMaker;
-  }
-
-  selectCoffee() {
-    this.drinkType = "C";
-    this.sugar = 0;
-    this.order = new ProcessingOrder("C")
   }
 
   makeDrink(): void {
@@ -39,35 +31,27 @@ export class CoffeeMachine {
   }
 
   private createCommand() {
-    if (this.drinkType === "") {
-      return `M:drink-has-not-been-selected`
-    }
     if (!this.order) {
       return `M:drink-has-not-been-selected`
-    }
-
-    if (this.sugar > 0) {
-      return `${this.drinkType}:${this.sugar}:0`
     }
 
     return this.order.toCommand()
   }
 
+  selectCoffee() {
+    this.order = new ProcessingOrder("C")
+  }
+
 
   selectTea() {
-    this.drinkType = "T"
-    this.sugar = 0;
     this.order = new ProcessingOrder("T")
   }
 
   selectChocolate() {
-    this.drinkType = "H"
-    this.sugar = 0;
     this.order = new ProcessingOrder("H")
   }
 
   addSugar() {
-    this.sugar += 1
     return this.order?.addSugar()
   }
 }
